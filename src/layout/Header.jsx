f7fa92702217ceecc665123f4854b20ac2c0403c
shlein <optime15022013@mail.ru>
2021-07-27 03:33:22 +0800

    gh-pages added

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 			<div className="nav-wrapper">
 				<a href="#!" className="brand-logo">React showcase</a>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
-					<li><a href="!#">Repo</a></li>
+					<li><a href="https://github.com/Shlein/react-shop" target='_blank' rel='noreferrer'>Repo</a></li>
 				</ul>
 			</div>
 		</nav>
